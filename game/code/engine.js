7c455ae2f69d1b85d331cfcbda01ec3db4c68f84
KargJonas <jonas.karg@icloud.com>
2018-02-08 23:28:26 +0100

    Bug fixing, performance, expandability, ..

--- game/code/engine.js
+++ game/code/engine.js
@@ -1,51 +1,65 @@
+// Settings
+var rows = 9,           // Amount of texture-tiles lenghtwise
+    columns = 13,       // Amount of texture-tiles heightwise
+    size = 80,          // Resolution of the tiles ( pixels )
+    tiles_amount = 17;  // Amount of textures in the texture texture folder
+
 // Some variables
 var cnv = document.getElementById("game"),
     ctx = cnv.getContext("2d", {antialias: true}),
-    rows = 9, cols = 13, // Amount of rows and columns in the map
-    size = 80, // Size of each texture element
-    map, map_overlay,
-    start;
+    map, map_overlay, walls, start, x, y, tile_nr, tiles = [];
 
-// Temp
-var x, y,
-    tile_nr;
-
-// Map Tiles
-var tile_elems = [ "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10", "t11", "t12", "t13", "t14", "t15", "t16", "t17" ],
-    tiles = [];
-
+// Initiate the game
 function map_start() {
     start = new Date();
-    ctx.canvas.width = cols * size;
+    ctx.canvas.width = columns * size;
     ctx.canvas.height = rows * size;
-    for (var i = 0; i < tile_elems.length; i++) {
-        tiles[i] = document.getElementById(tile_elems[i]);
+    for (var i = 0; i < tiles_amount; i++) {
+        tiles[i] = new Image();
+        tiles[i].src = "resources/media/" + (i + 1) + ".png";
     }
-    map_update();
 }
 
+// Set the textures of all tiles
 function map_update() {
-    for (x = 0; x < cols; x++) {
+    for (x = 0; x < columns; x++) {
         for (y = 0; y < rows; y++) {
             tile_set(x, y);
         }
     }
 }
 
+// Set the texture of a tile
 function tile_set(x, y) {
-    tile_nr = map[y][x]; // Get the tile-nr
-    if (tile_nr > 0 && tile_nr < tile_elems.length + 1) {
+    tile_nr = map[y][x];
+    if (tile_nr > 0 && tile_nr < tiles_amount + 1) {
         ctx.drawImage(tiles[tile_nr - 1], x * size, y * size);
     }
-    
-    tile_nr = map_overlay[y][x]; // Get the tile-overlay-nr
-    if (tile_nr > 0 && tile_nr < tile_elems.length + 1) {
+    tile_nr = map_overlay[y][x];
+    if (tile_nr > 0 && tile_nr < tiles_amount + 1) {
         ctx.drawImage(tiles[tile_nr - 1], x * size, y * size);
     }
 }
 
-function millis() {
-    return new Date() - start;
+// Player
+function player () {
+    var x, y;
 }
+
+player.prototype.move = function(x, y) { // X and Y are the added positions
+    // this.x += x;  // We'll have to check first if the spot we want to go
+    // this.x += y;  // isn't blocked.
+    
+    // Keep efficiency in mind!!
+    
+    if () {
+        
+    }
+}
+
+// Get time passed ( milliseconds )
+function millis() { return new Date() - start; }
 
 map_start();
+window.onload = map_update;
+console.log("Loading time:", millis(), "ms");
